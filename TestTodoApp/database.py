22150906f032fb21b94596c2ba49b2d8082afca3
jimenatutillo <jimenatutillo91@gmail.com>
2025-09-30 11:11:14 -0500

    update script

--- TestTodoApp/database.py
+++ TestTodoApp/database.py
@@ -2,10 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# Postgres
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:admin@localhost/TodoApplicationDB'
-#
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
